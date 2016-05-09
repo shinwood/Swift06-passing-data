4276b51ded7ec3aefe4b2bae38f692714061a4a2
shinwood <shinwood@gmail.com>
2016-05-09 23:59:51 +0800

    1st commit

---
+++ Swift06-passing-data/Swift06-passing-data/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift06-passing-data
+//
+//  Created by shinwood on 5/9/16.
+//  Copyright © 2016 guox.in. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
